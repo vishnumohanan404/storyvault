43ccaf55ac7c2aa3d4485cd5ed030ea2c2b76e90
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2025-06-09 16:12:10 +0530

    name change

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="border-b flex justify-between">
+    <nav className="border-b flex justify-between sticky top-0 w-full z-10 backdrop-blur-lg ">
       <div className="container mx-auto px-6 py-4 flex justify-between">
-        <h4 className="text-xl font-bold capitalize">Svault</h4>
+        <h4 className="text-xl font-bold capitalize">Story</h4>
         {/* todo: searchbar + theme toggle */}
       </div>
     </nav>
