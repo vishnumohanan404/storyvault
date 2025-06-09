a74de481947791e2570abda9dadd6be36bc5713c
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2025-06-09 12:47:35 +0530

    manrope font added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Manrope, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const manropeSans = Manrope({
+  variable: "--font-manrope",
   subsets: ["latin"],
 });
 
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${manropeSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
