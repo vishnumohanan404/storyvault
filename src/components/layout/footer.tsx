d06ae8c8baca3461f7897c716fcaea1cad74585b
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2025-06-09 23:13:27 +0530

    footer icons added

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,4 +1,4 @@
-import { GithubIcon } from "lucide-react";
+import { GithubIcon, Globe2Icon, LinkedinIcon, TwitterIcon, XIcon } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
@@ -16,13 +16,27 @@ const Footer = () => {
               © 2025 Story. Built with Next.js and Tailwind CSS.
             </p>
           </div>
-          <div className="space-y-1.5">
-            <h4>Connect</h4>
+          <div className="flex gap-3 items-center">
             <Link
               className="text-muted-foreground"
               href={"https://github.com/vishnumohanan404/storyvault"}
+              target="_blank"
             >
               <GithubIcon className="w-5 h-5" />
+            </Link>
+            <Link
+              className="text-muted-foreground"
+              href={"https://vishnuvw.xyz/"}
+              target="_blank"
+            >
+              <Globe2Icon className="w-5 h-5" />
+            </Link>
+            <Link
+              className="text-muted-foreground"
+              href={"https://www.linkedin.com/in/404vishnu/"}
+              target="_blank"
+            >
+              <LinkedinIcon className="w-5 h-5" />
             </Link>
           </div>
         </div>
