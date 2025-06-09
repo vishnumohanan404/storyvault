44829189cce8934cb43aa439fc19766b54352e5f
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2025-06-09 16:12:26 +0530

    new shadcn components

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,34 @@
+import { GithubIcon } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="border border-t w-full">
+      <div className="container mx-auto p-6">
+        <div className="flex justify-between">
+          <div className="space-y-1.5">
+            <h4 className="font-semibold text-xl">Story</h4>
+            <p className="text-sm text-muted-foreground">
+              Interactive Project Narratives.
+            </p>
+            <p className="text-xs mt-4 text-muted-foreground">
+              © 2025 Story. Built with Next.js and Tailwind CSS.
+            </p>
+          </div>
+          <div className="space-y-1.5">
+            <h4>Connect</h4>
+            <Link
+              className="text-muted-foreground"
+              href={"https://github.com/vishnumohanan404/storyvault"}
+            >
+              <GithubIcon className="w-5 h-5" />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
