d9f1910d7224079bdfc6e65a79698002feadd25c
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2025-06-10 21:47:18 +0530

    kanban explore card created

---
+++ src/components/common/explore-cards/kanban.tsx
@@ -0,0 +1,24 @@
+import { ArrowRight } from "lucide-react";
+import React from "react";
+
+const ExploreKanban = ({ id }: { id: string }) => {
+  return (
+    <div className="rounded-lg border bg-card text-card-foreground shadow-sm p-6">
+      <div className="space-y-4">
+        <h3 className="font-semibold text-lg">Kanban</h3>
+        <p className="text-muted-foreground">
+          Kanban board template to track your progress using Agile Userstories
+        </p>
+        <a
+          className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 w-fit"
+          href={`/kanban/${id}`}
+        >
+          Try Kanban
+          <ArrowRight className="ml-2 h-4 w-4" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ExploreKanban;
