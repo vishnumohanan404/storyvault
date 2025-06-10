2eb7450e2abeac19a1dbb0124896a019c5abd33d
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2025-06-10 11:56:34 +0530

    projects query from sanity completed

--- src/app/(commonsidebar)/projects/page.tsx
+++ src/app/(commonsidebar)/projects/page.tsx
@@ -9,59 +9,40 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { client } from "@/sanity/client";
 import { ExternalLinkIcon, StarIcon } from "lucide-react";
+import { SanityDocument } from "next-sanity";
 import Link from "next/link";
 import React from "react";
 
-const projects = [
-  {
-    id: "1",
-    title: "GitOps AWS Pipeline",
-    description:
-      "Complete GitOps implementation on AWS with EKS, ArgoCD, and automated CI/CD pipeline.",
-    badges: ["AWS", "Kubernetes", "ArgoCD", "EKS", "GitLab"],
-    featured: true,
-    topic: "GitOps",
-    createdOn: new Date(8.64e15).toString(),
-  },
-  {
-    id: "2",
-    title: "Microservices on K8s",
-    description:
-      "Scalable microservices architecture with service mesh, monitoring, and observability.",
-    badges: ["Kubernetes", "Istio", "Prometheus"],
-    featured: false,
-    topic: "Architecture",
-    createdOn: new Date(8.64e15).toString(),
-  },
-  {
-    id: "3",
-    title: "Serverless Platform",
-    description:
-      "Event-driven serverless platform with AWS Lambda, API Gateway, and DynamoDB.",
-    badges: ["AWS Lambda", "API Gateway", "DynamoDB"],
-    featured: true,
-    topic: "Serverless",
-    createdOn: new Date(8.64e15).toString(),
-  },
-  {
-    id: "4",
-    title: "Cloud Resume Project",
-    description:
-      "A portfolio website hosted in S3 with AWS Lambda, API Gateway, CloudFront and DynamoDB.",
-    badges: ["AWS Lambda", "API Gateway", "DynamoDB", "S3"],
-    featured: true,
-    topic: "AWS",
-    createdOn: new Date(8.64e15).toString(),
-  },
-];
+const PROJECTS_QUERY = `*[_type == "projects"]{ 
+  _id, 
+  title, 
+  description,
+  badges,
+  topic,
+  featured,
+  createdOn }`;
 
-const ProjectsPage = () => {
+const options = { next: { revalidate: 30 } };
+type Project = {
+  _id: string;
+  title: string;
+  description: string;
+  badges: string[];
+  topic: string;
+  featured: boolean;
+  createdOn: string; // or Date if you parse it
+};
+
+const ProjectsPage = async () => {
+  const projects = await client.fetch<Project[]>(PROJECTS_QUERY, {}, options);
+  console.log('projects :>> ', projects);
   return (
     <div className="w-[100%]">
       <div className="grid grid-cols-3 gap-4">
         {projects.map((project) => (
-          <Card key={project.id} className="hover:shadow-accent">
+          <Card key={project._id} className="hover:shadow-accent">
             <CardHeader className="flex flex-col space-y-2">
               <CardAction className="flex justify-between gap-2">
                 {project.featured ? (
@@ -110,7 +91,7 @@ const ProjectsPage = () => {
             </CardContent>
             <CardFooter>
               <div className="flex gap-2">
-                <Link href={`/project/${project.id}`}>
+                <Link href={`/project/${project._id}`}>
                   <Button size="sm" className="hover:cursor-pointer">
                     View Docs
                   </Button>
